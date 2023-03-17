af08f860567809fbec5ec95b9eb317f31a4f6799
Daniel <iapyzov8@gmail.com>
2023-03-17 21:10:38 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,192 @@
+// const body = document.body
+
+
+// const input = document.createElement('input')
+// input.setAttribute('placeholder','Введите имя')
+
+// document.body.before(input)
+
+// const label = document.createElement('label')
+// label.innerHTML =`Имя:`
+
+// input.before(label)
+
+// const input1 = document.createElement('input')
+// input.setAttribute('label','имя')
+// input1.setAttribute('placeholder','Введите фамилию')
+
+
+
+// document.body.before(input1)
+
+// const label1 = document.createElement('label')
+// label1.innerHTML =`Фамилия:`
+// input1.before(label1)
+
+
+
+
+
+// const input2 = document.createElement('input')
+// input2.setAttribute('type','radio')
+
+
+
+// const label2 = document.createElement('label')
+// label2.innerHTML = `Мужчина:`
+
+// body.after(input2)
+
+
+// document.body.append(input2)
+// document.body.append(label2)
+// input2.before(label2)
+
+
+// const input3 = document.createElement('input')
+// input3.setAttribute('type','radio')
+
+
+// const label3 = document.createElement('label')
+// label3.innerHTML = `Женщина:`
+
+// body.after(input3)
+
+
+// document.body.append(input3)
+// document.body.append(label3)
+// input3.before(label3)
+
+
+
+// const text1 = document.createElement('p')
+// text1.innerHTML = `О себе`
+// document.body.append(text1)
+
+
+
+
+// const input4 = document.createElement('input')
+// input4.setAttribute('type','checkbox')
+
+// const label4 = document.createElement('label')
+// label4.innerHTML = `Меньше 18`
+
+// body.after(input4)
+
+// document.body.append(input4)
+// document.body.append(label4)
+// label4.before(input4)
+
+
+
+
+// const input5 = document.createElement('input')
+// input5.setAttribute('type','checkbox')
+
+// const label5 = document.createElement('label')
+// label5.innerHTML = `От 18 до 35`
+
+// document.body.append(input5)
+// document.body.append(label5)
+// label5.before(input5)
+
+
+
+// const input6 = document.createElement('input')
+// input6.setAttribute('type','checkbox')
+
+// const label6 = document.createElement('label')
+// label6.innerHTML = `Больше 35`
+
+// document.body.append(input6)
+// document.body.append(label6)
+// label6.before(input6)
+
+
+
+
+// const input7 = document.createElement('input')
+// input7.setAttribute('type','checkbox')
+
+// const label7 = document.createElement('label')
+// label7.innerHTML = `Женат/-а`
+
+// document.body.append(input7)
+// document.body.append(label7)
+// label7.before(input7)
+
+
+
+// const input8 = document.createElement('input')
+// input8.setAttribute('type','checkbox')
+
+
+// const label8 = document.createElement('label')
+// label8.innerHTML = `Есть кошка`
+
+// document.body.append(input8)
+// document.body.append(label8)
+// label8.before(input8)
+
+
+
+// const btn = document.createElement('button')
+// btn.innerHTML = `Отправить`
+// document.body.append(btn)
+
+
+// №2
+
+// const body = document.body
+// const h3 = document.createElement('h3')
+// h3.innerHTML ='Name:'
+// body.prepend(h3)
+
+
+// const input = document.createElement('input')
+// input.setAttribute("placeholder","Ваше имя" )
+// input.setAttribute("class","input" )
+// h3.after(input)
+
+
+// const h3b = document.createElement('h3')
+// h3b.innerHTML ='Number:'
+// input.after(h3b)
+
+
+// const input2 = document.createElement('input')
+// input2.setAttribute("placeholder","Number" )
+// input2.setAttribute("class","input" )
+// h3b.after(input2)
+
+
+
+// const h3c = document.createElement('h3')
+// h3c.innerHTML ='Email:'
+// input.after(h3c)
+
+
+// const input3 = document.createElement('input')
+// input3.setAttribute("placeholder","Email" )
+// input3.setAttribute("class","input" )
+// h3c.after(input3)
+
+
+
+// const h3d = document.createElement('h3')
+// h3d.innerHTML ='Sum:'
+// input.after(h3d)
+
+
+// const input4 = document.createElement('input')
+// input4.setAttribute("placeholder","Sum" )
+// input4.setAttribute("class","input" )
+// h3d.after(input4)
+
+
+
+// const btn = document.createElement('button')
+// btn.innerHTML ='Go to the payment'
+// btn.setAttribute("class","btn")
+// input4.after(btn)
